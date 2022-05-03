5b9144ffe1d6332343ddc0dc39853fa8eb4e833a
shane schmaltz <stschmaltz@gmail.com>
2022-05-02 23:08:41 -0600

    add player movement

---
+++ Assets/Player.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class Player : MonoBehaviour
+{
+
+    [SerializeField] float moveSpeed = 5f;
+    Vector2 rawInput;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 delta = rawInput * moveSpeed * Time.deltaTime;
+        transform.position += delta;
+    }
+
+    void OnMove(InputValue value)
+    {
+        rawInput = value.Get<Vector2>();
+        Debug.Log(rawInput);
+    }
+}
