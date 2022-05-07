6ba9b403c4e852cda3dc581e6ee2a5512f446a10
shane schmaltz <stschmaltz@gmail.com>
2022-05-06 23:49:32 -0600

    add health bars

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,42 +1,81 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.UI;
+using UnityEngine.UI;
 
 public class Health : MonoBehaviour
 {
-    [SerializeField] int health = 100;
+    [SerializeField] float maxHealth = 100;
+    [SerializeField] float currentHealth;
+    [SerializeField] Slider healthBarSlider;
+    [SerializeField] Canvas healthBarUI;
 
-    public int GetHealth()
+
+    void Start()
     {
-        return health;
+        currentHealth = maxHealth;
+        // Debug.Log("OI" + healthBarSlider != null);
+        if (healthBarSlider != null && healthBarUI != null)
+        {
+            healthBarUI.enabled = false;
+            healthBarSlider.value = CalculateHealthValue();
+        }
+    }
+
+    void Update()
+    {
+        // Debug.Log("currentHealth: " + currentHealth);
+        // Debug.Log("maxHealth: " + maxHealth);
+
+        if (healthBarSlider != null)
+        {
+            Debug.Log(CalculateHealthValue());
+
+            healthBarSlider.value = CalculateHealthValue();
+        }
+
+        if (healthBarUI != null && currentHealth < maxHealth)
+        {
+            healthBarUI.enabled = true;
+        }
+
+        if (currentHealth > maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+
+
+    }
+
+    public float CalculateHealthValue()
+    {
+        return currentHealth / maxHealth * 100;
     }
 
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        // Debug.Log("yoyoyo" + GetHealth());
-
         DamageDealer damageDealer = other.GetComponent<DamageDealer>();
-        Debug.Log(damageDealer != null);
         Debug.Log(other.name);
+        Debug.Log("yoz");
+        Debug.Log(damageDealer != null);
 
 
         if (damageDealer != null)
         {
-
-            Debug.Log("zzz" + damageDealer);
 
             TakeDamage(damageDealer.GetDamage());
             damageDealer.Hit();
         }
     }
 
-    void TakeDamage(int damage)
+    void TakeDamage(float damage)
     {
         Debug.Log("you took " + damage + " damage");
-        health -= damage;
+        currentHealth -= damage;
 
-        if (health <= 0)
+        if (currentHealth <= 0)
         {
             Destroy(gameObject);
         }
