de93d78d48c2970992be534925053d1cb3e9e9ab
shane schmaltz <stschmaltz@gmail.com>
2022-05-12 23:13:43 -0600

    re-add autosave

---
+++ Assets/AutoSave/Editor/AutoSaveExtension.cs
@@ -0,0 +1,26 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+
+namespace EckTechGames
+{
+    [InitializeOnLoad]
+    public class AutoSaveExtension
+    {
+        // Static constructor that gets called when unity fires up.
+        static AutoSaveExtension()
+        {
+            EditorApplication.playmodeStateChanged += AutoSaveWhenPlaymodeStarts;
+        }
+
+        private static void AutoSaveWhenPlaymodeStarts()
+        {
+            // If we're about to run the scene...
+            if (EditorApplication.isPlayingOrWillChangePlaymode && !EditorApplication.isPlaying)
+            {
+                // Save the scene and the assets.
+                EditorSceneManager.SaveOpenScenes();
+                AssetDatabase.SaveAssets();
+            }
+        }
+    }
+}
